224e1fdb90571a249f4bdd77ee667e10f1c46022
Chien-LV <144431349+Chien-LV@users.noreply.github.com>
2024-01-12 22:54:28 +0700

    fix sths

--- src/components/Layouts/components(dungchung)/Nav-bar/index.js
+++ src/components/Layouts/components(dungchung)/Nav-bar/index.js
@@ -11,8 +11,10 @@ const NavBar = () => {
             <div className="max-w-7xl mx-auto px-2 sm:px-6 lg:px-8">
                 <div className="relative flex items-center justify-between h-16">
                     <div className="flex-1 flex items-center justify-start">
-                        <div className="flex-shrink-0 flex items-center">
-                            <i className="fas fa-home text-white text-2xl"></i>
+                        <div className="flex-shrink-0 flex items-center ml-2">
+                            <Link to={configs.routes.home}>
+                                <i className="fas fa-home text-white text-2xl"></i>
+                            </Link>
                             <div className="hidden md:block">
                                 <div className="ml-10 flex items-baseline space-x-4">
                                     <Link
